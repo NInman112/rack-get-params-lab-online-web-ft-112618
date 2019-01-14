747ba535e90af314011b4977c93e13cf48d7485e
Nick Inman <nick@theoatmeal.com>
2019-01-14 19:47:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -32,7 +32,6 @@ class Application
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
@@ -43,4 +42,5 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+  
 end
